f0d2d2c8baebcb433778b1ada9f21362641413d8
Bill Wang <bill.wang@morningstar.com>
2019-06-09 22:48:59 -0500

    add unit test

---
+++ src/test/java/us/temperature/services/GeoHashServiceImplTest.java
@@ -0,0 +1,12 @@
+package us.temperature.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GeoHashServiceImplTest {
+
+    @Test
+    public void getGeoHashId() {
+    }
+}
